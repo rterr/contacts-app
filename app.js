0e6f28e23cd68142091859562ae5e9650ae43c0d
R. Terrado <r.terrado@gmail.com>
2016-07-22 13:08:36 -0700

    Finished Version 1

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-$(document).ready({
+$(document).ready(function(){
 
 	function Contactscreator(firstName, lastName, phone, street, city, state) {
 	 
@@ -28,13 +28,19 @@ $(document).ready({
 		$('#input-city').val(), 
 		$('#input-state').val()
 		);
-		console.log(counter + ' firstname: ' + contactsArray[counter].firstName);
-		console.log(counter + ' phone: ' + contactsArray[counter].phone);
-		console.log(counter + ' state: ' + contactsArray[counter].state);
+		$("#contacts-ul").append("<li class='contacts-li' id='" + counter + "'>" + contactsArray[counter].firstName + " " + contactsArray[counter].lastName + "</li>");
 		counter++;
 		$("form")[0].reset();
-	
-		
-	})
+		$("li.contacts-li").click(function(e){
+			e.preventDefault();
+			$('#sidebar-fullname').text(contactsArray[event.target.id].firstName + " " + contactsArray[event.target.id].lastName);
+			$('#sidebar-firstname').text("First name: " + contactsArray[event.target.id].firstName);
+			$('#sidebar-lastname').text("Last name: " + contactsArray[event.target.id].lastName);
+			$('#sidebar-phone').text("Phone number: " + contactsArray[event.target.id].phone);
+			$('#sidebar-address').text("Address: " + contactsArray[event.target.id].street + ", " + contactsArray[event.target.id].city + " " + contactsArray[event.target.id].state);
+		});
+	});
 
-})
+//end
+});
+
